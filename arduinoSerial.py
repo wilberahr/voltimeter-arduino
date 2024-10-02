f8e66fd04a53d4403132e191fc8470f09118d5eb
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2024-10-02 08:11:41 -0600

    Serial port connection to arduino and data read and save in cvs file.

--- arduinoSerial.py
+++ arduinoSerial.py
@@ -15,3 +15,25 @@ for port in ports:
 file = open("voltages_data.csv", "w", newline=' ')
 file.truncate
 
+#Connect to serial port
+serialCom = serial.Serial('COM3',9600)
+
+#Reset arduino
+serialCom.setDIR(False)
+time.sleep(1)
+serialCom.flushInput()
+serialCom.setDIR(True)
+
+data_points_max = 100
+
+for line in range(data_points_max):
+     try:
+          read_bytes = serialCom.readline()
+          decoded_data = read_bytes.decode('utf-8').strip('\r\n')
+          print(decoded_data)
+
+     except:
+          print("Error: data not saved.")
+
+#Closing csv file
+file.close()
